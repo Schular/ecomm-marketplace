f00233557fd69459651139ea2081b4ec0c71b609
schular <sular_paul@yahoo.com>
2024-06-08 14:35:57 +0300

    feat(component): update types, add table component, add typescript eslint

---
+++ src/types/emag/product.ts
@@ -0,0 +1,25 @@
+export type Product = {
+  ext_part_number: string;
+  part_number_key: string;
+  retained_amount: number;
+  sale_price: string;
+  created: string;
+  modified: string;
+  original_price: string;
+  id: number;
+  product_id: string;
+  part_number: string;
+  currency: string;
+  mkt_id: number;
+  name: string;
+  quantity: number;
+  vat: string;
+  status: number;
+  attachments: unknown[];
+  initial_qty: number;
+  storno_qty: number;
+  reversible_vat_charging: number;
+  recycle_warranties: unknown[];
+  product_voucher_split: unknown[];
+  details: unknown[];
+};
